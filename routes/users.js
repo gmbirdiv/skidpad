49d95445fa4384b120057fe814a482dfdd786933
George M Bird <gmbirdiv@gmail.com>
2020-03-12 14:21:59 -0400

    merged

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../model/userModel');
+const bcrypt = require('bcryptjs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res, next) {
+  res.render('template', {
+    locals: {
+      title: 'User Sign Up',
+      is_logged_in: req.session.is_logged_in
+    },
+    partials: {
+      partial: 'partial-signup'
+    }
+  });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('template', {
+    locals: {
+      title: 'User Login',
+      is_logged_in: req.session.is_logged_in
+    },
+    partials: {
+      partial: 'partial-login'
+    }
+  });
+});
+
+router.post('/login', async function(req, res, next) {
+  const { username, password } = req.body;
+
+  const user = new UserModel(null, username, null, password);
+  const loginResponse = await user.loginUser();
+  console.log('loginResponse is', loginResponse.isValid);
+
+  console.table(loginResponse);
+
+  if (!!loginResponse.isValid) {
+    req.session.is_logged_in = loginResponse.isValid;
+    req.session.user_id = loginResponse.user_id;
+    req.session.username = loginResponse.username;
+    res.redirect('/');
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+
+
+
+const { check, validationResult } =  require('express-validator');
+
+router.post('/signup',[
+  check ('username').isLength({min: 1}).withMessage('Must have a username'),
+  check ('email').isLength({min: 1}).withMessage('Must have an email address'), 
+  check ('password').isLength({min: 1}).withMessage('Must have a password')
+], async function(req,res,next){
+  
+  const { username, email, password} = req.body; 
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }  
+  const user = new UserModel(null,username, email, hash);
+  user.addUser(); 
+  res.sendStatus(200);
+});
+
+router.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
 });
 
 module.exports = router;
