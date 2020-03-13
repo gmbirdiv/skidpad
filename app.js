c399751cb6b73c4786db5363e0fd800fcc549254
George M Bird <gmbirdiv@gmail.com>
2020-03-12 23:03:06 -0400

    cmon

--- app.js
+++ app.js
@@ -49,14 +49,5 @@ app.use('/users', usersRouter);
 app.use('/vehicles', vehiclesRouter);
 app.use('/reviews', reviewRouter);
 
-const getMakes  = async () => {
-  return await axios ({
-    url: 'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/honda/modelyear/2015/vehicleType/passenger?format=json'
-  })
 
-}
-
-(async() => {
-console.log(await getMakes())
-})()
 module.exports = app;
