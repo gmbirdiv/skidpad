711d664687271c0ce7c05995f768714690ed90d4
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-14 16:56:15 -0400

    getting closer on error msg's for validation

--- validator.js
+++ validator.js
@@ -1,3 +1,18 @@
+const express = require('express'),
+  session = require('express-session'),
+  FileStore = require(`session-file-store`)(session),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  flash = require('connect-flash'),
+  expressValidator = require('express-validator'),
+  es6Renderer = require('express-es6-template-engine');
+
+const indexRouter = require('./routes/index'),
+  vehiclesRouter = require('./routes/vehicles'),
+  reviewRouter = require('./routes/reviews'),
+  usersRouter = require('./routes/users');
+
 const { body, validationResult } = require('express-validator');
 const userValidationRules = () => {
   return [
@@ -12,14 +27,22 @@ const userValidationRules = () => {
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
+  console.log('validate :', errors);
   if (errors.isEmpty()) {
     return next();
   }
   const extractedErrors = [];
   errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
-
-  return res.status(422).json({
-    errors: extractedErrors
+  console.log(extractedErrors);
+  return res.render('template', {
+    locals: {
+      title: 'Sign up',
+      errors: extractedErrors,
+      is_logged_in: req.session.is_logged_in
+    },
+    partials: {
+      partial: 'partial-signupfail'
+    }
   });
 };
 
